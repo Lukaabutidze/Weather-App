2cc62fcb086a6740f04d27657d7695ff2859b7c3
Luka Abutidze <lukaabutidze99@gmail.com>
2024-02-25 21:26:07 +0400

    visual changes and added local time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,170 @@
-import Image from "next/image";
+"use client";
+
+import { useState, useEffect, useRef } from "react";
 
 export default function Home() {
+  const [location, setLocation] = useState<string>("");
+  const [weather, setWeather] = useState<JSX.Element | null>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  // Focus to be on input field
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+
+  // To search Location with Enter key
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      getWeather();
+    }
+  };
+
+  const getWeather = async () => {
+    const api_key = "53d47eb8c0f34a5387c144317242302";
+    const api_url =
+      "http://api.weatherapi.com/v1/current.json?key=" +
+      api_key +
+      "&q=" +
+      location;
+
+    if (location) {
+      try {
+        const res = await fetch(api_url);
+        const data = await res.json();
+
+        if (data.error) {
+          setWeather(<h1>Please type an existing location...</h1>);
+        } else {
+          const api_data = {
+            country: data.location.country,
+            city: data.location.name,
+            temp: data.current.temp_c,
+            humidity: data.current.humidity,
+            wind: data.current.wind_kph,
+            gust: data.current.gust_kph,
+            visibility: data.current.vis_km,
+            condition: data.current.condition.text,
+            img: data.current.condition.icon,
+            date: data.location.localtime,
+          };
+
+          setWeather(
+            <>
+              <div className="text-center text-2xl p-1  font-bold">
+                {api_data.city}
+              </div>
+              <div className="flex justify-center">
+                <div className="flow-root">
+                  <div className="float-left">
+                    <img
+                      src={api_data.img}
+                      width="90"
+                      alt="Condition"
+                      className="opacity-90"
+                    />
+                  </div>
+                  <div className="float-left text-6xl text-sky-600 font-semibold degrees mt-4 opacity-85">
+                    {api_data.temp}
+                  </div>
+                </div>
+              </div>
+              <div className="text-center text-gray-600">
+                {api_data.condition}
+              </div>
+              <div className="flow-root p-2 mt-4">
+                <div className="float-left text-gray-600 ">
+                  Humidity: {api_data.humidity} %
+                </div>
+                <div className="float-right text-gray-600 ">
+                  Wind: {api_data.wind} Kph{" "}
+                </div>
+                <div className="float-left text-gray-600 ">
+                  Visibility: {api_data.visibility} km{" "}
+                </div>
+                <div className="float-right text-gray-600 ">
+                  Gust: {api_data.gust} Kph
+                </div>
+              </div>
+              <div className="mt-9 p-1 ml-auto float-end text-xs text-gray-800 font-bold">
+                Local Time: {api_data.date}
+              </div>
+            </>
+          );
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      //
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <nav className="flex items-center justify-center py-4 bg-transparent w-full m-0 opacity-90">
+        <div className="relative">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-plus-circle"
+              viewBox="0 0 16 16"
+            >
+              {" "}
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />{" "}
+              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />{" "}
+            </svg>
+          </div>
+          <input
+            className="block bg-white text-black rounded-lg opacity-70 pl-10 p-3 placeholder:font-bold capitalize w-full"
+            type="text"
+            id="location"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            placeholder="Search For Location..."
+            onKeyDown={handleKeyDown}
+            ref={inputRef}
+          />
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          className="bg-blue-500 hover:bg-blue-900 text-white font-bold m-2 p-5 rounded-lg"
+          id="search"
+          onClick={getWeather}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <svg
+            className="w-5 h-5"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            ></path>
+          </svg>
+          <span className="sr-only">Search</span>
+        </button>
+      </nav>
+      {weather && (
+        <div className="flex w-full p-20 justify-center">
+          <div className="w-full max-w-xs">
+            <div className="mb-5">
+              <div className="bg-white shadow-xl rounded-3xl px-8 pt-6 mb-4 opacity-90 min-h-[330px]  mt-8 py-4">
+                {weather}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
